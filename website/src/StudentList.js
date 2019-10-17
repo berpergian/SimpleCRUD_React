6a4bfbf69d190ec876defff6e5c05067bb065be1
Muhammad Ganjar <ganjar_ibrahimovic@rocketmail.com>
2019-10-17 14:47:46 +0700

    Add create component

--- website/src/StudentList.js
+++ website/src/StudentList.js
@@ -21,23 +21,23 @@ const online_data = "https://api.jsonbin.io/b/5d9c2479ee799949207cb48f"
 const offline_data = "http://localhost:4000/student"
 
 const tableIcons = {
-	Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
 	Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
 };
 
 class StudentList extends React.Component {
@@ -45,14 +45,16 @@ class StudentList extends React.Component {
     	super(props);
     	this.state = { 
     		columns: [
-    			{ title: 'No', field: 'id'},
+    			{ title: 'No', field: 'id', editable: 'never'},
     			{ title: 'Name', field: 'name'},
     			{ title: 'Address', field: 'address'},
     		],
     		data: [
     			/*{ id: 1, name: 'Mehmet', address: 'Baran'},
         		{ id: 2, name: 'Zerya Betül', address: 'dasdsadsa'},*/
-    		]
+    		],
+        name: '',
+        address: '',
     	};
   	}
 
@@ -61,6 +63,30 @@ class StudentList extends React.Component {
   			.then(response  => response.json())
   			.then(data => (this.setState({data})));
   	}
+
+    addStudent(inputData) {
+      var data = {
+        name: inputData.name,
+        address: inputData.address
+      }
+
+      const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      }
+
+      fetch("http://localhost:4000/addstudent", options).then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      }).then(function(data) {
+        console.log(data);
+      }).catch(function(err){
+        console.log(err);
+      })
+    }
 
   	render() {
   		return(
@@ -76,6 +102,8 @@ class StudentList extends React.Component {
   								{
   									const data = this.state.data;
   									data.push(newData);
+                    console.log(newData);
+                    this.addStudent(newData);
   									this.setState({ data }, () => resolve());
   								}
   								resolve()
@@ -107,7 +135,7 @@ class StudentList extends React.Component {
   						}),
   				}}
   				options={{
-  					paging: false
+  					paging: true
   				}}
   			/>
   		)
